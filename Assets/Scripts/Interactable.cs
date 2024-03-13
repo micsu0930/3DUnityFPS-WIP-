a4cd49c3d846a21402f86029577b7e3f86685bd8
Micsu <mihalyi144@gmail.com>
2024-03-13 20:40:42 +0100

    Implement Health and editor sscripts

--- Assets/Scripts/Interactable.cs
+++ Assets/Scripts/Interactable.cs
@@ -1,13 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public abstract class Interactable : MonoBehaviour
 {
+    public bool useEvents;
+    [SerializeField]
     public string promptMessage;
     
+    public virtual string OnLook()
+    {
+        return promptMessage;
+    }
     public void BaseInteract()
     {
+        if(useEvents)
+        {
+            GetComponent<InteractionEvent>().OnInteract.Invoke();
+        }
         Interact();
         //function will be called from player
     }
